26ce5fdd0f9a001a4ae50634f617a10ea2715ef4
mzhao99 <zhao.man@northeastern.edu>
2023-04-08 19:20:30 -0400

    main structure

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("api")
+    .then(response => response.json())
+    .then(data => setBackendData(data))
+  },[]);
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i} className="text-3xl font-bold underline">{user}</p>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default App
