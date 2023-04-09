98cfc7acf0fd3991dbceff00abf77d2224a3d9fe
mzhao99 <zhao.man@northeastern.edu>
2023-04-09 15:34:07 -0400

    main structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [backendData, setBackendData] = useState([{}]);
+    const [backendData, setBackendData] = useState([{}]);
 
-  useEffect(() => {
-    fetch("api")
-    .then(response => response.json())
-    .then(data => setBackendData(data))
-  },[]);
-  return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        backendData.users.map((user, i) => (
-          <p key={i} className="text-3xl font-bold underline">{user}</p>
-        ))
-      )}
-    </div>
-  )
+    useEffect(() => {
+        fetch("api")
+        .then((response) => response.json())
+        .then((data) => setBackendData(data));
+    }, []);
+    return (
+        <div>
+        {typeof backendData.users === "undefined" ? (
+            <p>Loading...</p>
+        ) : (
+            backendData.users.map((user, i) => (
+            <p key={i} className="text-3xl font-bold underline">
+                {user}
+            </p>
+            ))
+        )}
+        </div>
+    );
 }
 
-export default App
+export default App;
