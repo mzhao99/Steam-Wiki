6e04fb5937c04564e079929d1c3cc221a2d695f0
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2023-08-09 14:50:03 +0800

    modified user controller

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -1,25 +1,47 @@
+const asyncHandler = require('express-async-handler');
+const mongoose = require('mongoose');
+const User = require('../../models/userModel');
+
 // @desc Create User
 // @route /users
-const createUser = (req, res) => {
-    res.status(200).json({message: "Add new user"});
-};
+const createUser = asyncHandler(async (req, res) => {
+    const body = req.body;
+    const user = await User.create({
+        username: body.username,
+        password: body.password,
+        email: body.email,
+        favorites: body.favorites
+    });
+    res.status(200).json(user);
+});
 
 // @desc Get Favorites
 // @route /users/favorites
-const getFavorites = (req, res) => {
-    res.status(200).json({message: "Get user favorite games"});
-};
+const getFavorites = asyncHandler(async (req, res) => {
+    const user_id = req.body.username;
+    try {
+        const user = await User.findById(user_id).populate('favorites');
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        const favorites = user.favorites;
+        res.status(200).json(favorites);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server error'}); 
+    }
+});
 
 // @desc Add Favorites
 // @route /users/favorites
-const addToFavorites = (req, res) => {
+const addToFavorites = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Add user favorite games"});
-};
+});
 
 // @desc Remove Favorites
 // @route /users/favorites/:game_id
-const removeFavorite = (req, res) => {
+const removeFavorite = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Remove user favorite games"});
-};
+});
 
 module.exports = { createUser, getFavorites, addToFavorites, removeFavorite };
