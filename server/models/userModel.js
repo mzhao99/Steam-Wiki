7661a08de8e44286dfcb0b1aa6b26382421b6826
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2023-08-09 12:56:35 +0800

    implemented user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,24 @@
 // User model definition
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    favorites: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Game'
+    }]
+});
+
+module.exports = mongoose.model('User', userSchema);
