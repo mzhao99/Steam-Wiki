16d576707896bd0c5cc7f5d983bd640264b6755a
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2024-02-03 16:41:15 -0500

    complete image upload functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,81 @@
 import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
 
 export default function Profile() {
+    const fileRef = useRef(null);
+    const dispatch = useDispatch();
     const { currentUser, loading, error } = useSelector((state) => state.user);
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+    const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
+
+    // for image upload
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+    
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePerc(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+                    setFormData({ ...formData, avatar: downloadURL })
+                );
+            }
+        );
+    };
+
+    // update the state
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+    
 
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
             <form className='flex flex-col gap-4'>
-                <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+                {/* change avatar */}
+                <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*' />
+                <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+                {/* error message for image upload */}
+                <p className='text-sm self-center'>
+                    {fileUploadError ? (
+                        <span className='text-red-700'>Error image upload (image must be less than 2 mb)</span>
+                    ) : filePerc > 0 && filePerc < 100 ? (
+                        <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+                    ) : filePerc === 100 ? (
+                        <span className='text-green-700'>Image successfully uploaded!</span>
+                    ) : (
+                        ''
+                    )}
+                </p>
+                
                 <input type='text' placeholder='Username' defaultValue={currentUser.username} id='username' className='border p-3 rounded-lg' />
                 <input type='email' placeholder='Email' id='email' defaultValue={currentUser.email} className='border p-3 rounded-lg' />
-                <input type='password' placeholder='Password' id='password' className='border p-3 rounded-lg'
-                />
+                <input type='password' placeholder='Password' id='password' className='border p-3 rounded-lg' />
                 <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80' >
                     {loading ? 'Loading...' : 'Update'}
                 </button>
